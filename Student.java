52902e92c621516f523e9984857f3eec2a4e89fd
ElizabetaSh <elizabeta.shljupkovska@students.finki.ukim.mk>
2020-04-12 21:19:15 +0200

    Student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,53 @@
+import java.util.List;
+
 class Student {
-	String index;
-	String firstName;
-	String lastName;
-	List <integer> grades
+String index;
+String firstName;
+String lastName;
+List<Integer> grades;
 
-	//TODO constructor
+//TODO constructor
 
-	//TODO seters & getters
+public Student(String index, String firstName, String lastName, List<Integer> grades) {
+this.index = index;
+this.firstName = firstName;
+this.lastName = lastName;
+this.grades = grades;
+}
 
-	public double getAverage() {
-		//TODO
-	}
+//TODO seters & getters
+public String getIndex() {
+return index;
+}
 
-	public int ECTSCredits() {
-		//TODO
-	}
+public String getFirstName() {
+return firstName;
+}
+
+public String getLastName() {
+return lastName;
+}
+
+public List<Integer> getGrades() {
+return grades;
+}
+public double getAverage() {
+//TODO
+double oceni=0;
+for(int i : grades)
+{
+oceni+=i;
+}
+oceni=oceni/grades.size();
+return oceni;
+}
+
+public int ECTSCredits() {
+//TODO
+int crediti=0;
+for (int i : grades){
+if (i>5) crediti+=6;
+}
+return crediti;
+}
 }
